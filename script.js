3ad348aa7acf5fe02b5c47969cd4e7471e2a5a22
LovelydoveyDaryle <127905053+LovelydoveyDaryle@users.noreply.github.com>
2023-05-25 16:27:43 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector(".Water");
+
+
+button.onclick = function() {
+    let sus = document.querySelector(".melon").value;
+    
+    
+    
+    let weird  = document.querySelector(".calm").value;
+    
+console.log(sus);
+    
+};
